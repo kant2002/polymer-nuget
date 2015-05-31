f5cec87dbddc22a3c0d105abfbb41bee91623105
Andrey Kurdyumov <kant2002@gmail.com>
2015-05-31 11:32:16 +0600

    Update packages for Polymer and Iron elements
    All elements now moved to separate packages

--- build.ps1
+++ build.ps1
@@ -1,114 +1,93 @@
 function Update-Nuget {
     param([string]$nugetPackage, [string]$bowerPackage)
         
-    rm $nugetPackage/content/Scripts/Polymer/$bowerPackage -Recurse -Force
-    cp bower_components/$bowerPackage $nugetPackage/content/Scripts/Polymer/ -Recurse -Force
+    rm $nugetPackage/content/Scripts/$bowerPackage -Recurse -Force
+    cp bower_components/$bowerPackage $nugetPackage/content/Scripts/ -Recurse -Force
 	rm bower_components/$bowerPackage -Recurse -Force
-}
-
-function Update-Platform() {
-    Update-Nuget "polymer-platform" "platform"
-}
-
-function Update-Polymer-Cyclic() {
-    Update-Nuget "polymer-cyclic" "polymer"
-    Update-Nuget "polymer-cyclic" "webcomponentsjs"
 }
 
 function Update-Polymer() {
     # Do nothing since this everything in polymer-ciclic
+    Update-Nuget "polymer" "polymer"
+    Update-Nuget "polymer" "webcomponentsjs"
 }
 
-function Update-Polymer-Core-Component-Page() {
-    Update-Nuget "polymer-core-component-page" "core-component-page"
+function Update-Polymer-Iron-Component-Page() {
+    Update-Nuget "polymer-iron-component-page" "iron-doc-viewer"
+    Update-Nuget "polymer-iron-component-page" "iron-flex-layout"
+    Update-Nuget "polymer-iron-component-page" "paper-toolbar"
+    Update-Nuget "polymer-iron-component-page" "hydrolysis"
+    Update-Nuget "polymer-iron-component-page" "paper-header-panel"
+    Update-Nuget "polymer-iron-component-page" "iron-icons"
+    Update-Nuget "polymer-iron-component-page" "iron-selector"
+    Update-Nuget "polymer-iron-component-page" "paper-styles"
+    Update-Nuget "polymer-iron-component-page" "iron-component-page"
 }
 
-function Update-Polymer-Core-Elements() {
-    Update-Nuget "polymer-core-elements" "context-free-parser"
-    Update-Nuget "polymer-core-elements" "core-ajax"
-    Update-Nuget "polymer-core-elements" "core-animated-pages"
-    Update-Nuget "polymer-core-elements" "core-animation"
-    Update-Nuget "polymer-core-elements" "core-collapse"
-    Update-Nuget "polymer-core-elements" "core-doc-viewer"
-    Update-Nuget "polymer-core-elements" "core-docs"
-    Update-Nuget "polymer-core-elements" "core-drag-drop"
-    Update-Nuget "polymer-core-elements" "core-drawer-panel"
-    Update-Nuget "polymer-core-elements" "core-dropdown"
-    Update-Nuget "polymer-core-elements" "core-dropdown-menu"
-    Update-Nuget "polymer-core-elements" "core-elements"
-    Update-Nuget "polymer-core-elements" "core-field"
-    Update-Nuget "polymer-core-elements" "core-focusable"
-    Update-Nuget "polymer-core-elements" "core-header-panel"
-    Update-Nuget "polymer-core-elements" "core-icon"
-    Update-Nuget "polymer-core-elements" "core-icon-button"
-    Update-Nuget "polymer-core-elements" "core-icons"
-    Update-Nuget "polymer-core-elements" "core-iconset"
-    Update-Nuget "polymer-core-elements" "core-iconset-svg"
-    Update-Nuget "polymer-core-elements" "core-image"
-    Update-Nuget "polymer-core-elements" "core-input"
-    Update-Nuget "polymer-core-elements" "core-item"
-    Update-Nuget "polymer-core-elements" "core-label"
-    Update-Nuget "polymer-core-elements" "core-layout-grid"
-    Update-Nuget "polymer-core-elements" "core-layout-trbl"
-    Update-Nuget "polymer-core-elements" "core-list"
-    Update-Nuget "polymer-core-elements" "core-localstorage"
-    Update-Nuget "polymer-core-elements" "core-media-query"
-    Update-Nuget "polymer-core-elements" "core-menu"
-    Update-Nuget "polymer-core-elements" "core-menu-button"
-    Update-Nuget "polymer-core-elements" "core-meta"
-    Update-Nuget "polymer-core-elements" "core-overlay"
-    Update-Nuget "polymer-core-elements" "core-pages"
-    Update-Nuget "polymer-core-elements" "core-range"
-    Update-Nuget "polymer-core-elements" "core-resizable"
-    Update-Nuget "polymer-core-elements" "core-scaffold"
-    Update-Nuget "polymer-core-elements" "core-scroll-header-panel"
-    Update-Nuget "polymer-core-elements" "core-scroll-threshold"
-    Update-Nuget "polymer-core-elements" "core-selection"
-    Update-Nuget "polymer-core-elements" "core-selector"
-    Update-Nuget "polymer-core-elements" "core-shared-lib"
-    Update-Nuget "polymer-core-elements" "core-signals"
-    Update-Nuget "polymer-core-elements" "core-splitter"
-    Update-Nuget "polymer-core-elements" "core-style"
-    Update-Nuget "polymer-core-elements" "core-tests"               # Probably have to remove that.
-    Update-Nuget "polymer-core-elements" "core-toolbar"
-    Update-Nuget "polymer-core-elements" "core-tooltip"
-    Update-Nuget "polymer-core-elements" "core-transition"
-    Update-Nuget "polymer-core-elements" "font-roboto"
-    Update-Nuget "polymer-core-elements" "google-code-prettify"
-    Update-Nuget "polymer-core-elements" "marked"
-    Update-Nuget "polymer-core-elements" "marked-element"
-    Update-Nuget "polymer-core-elements" "paper-button"
-    Update-Nuget "polymer-core-elements" "paper-fab"
-    #Update-Nuget "polymer-core-elements" "paper-focusable"
-    Update-Nuget "polymer-core-elements" "paper-icon-button"
-    Update-Nuget "polymer-core-elements" "paper-ripple"
-    Update-Nuget "polymer-core-elements" "paper-shadow"
-    Update-Nuget "polymer-core-elements" "polymer-test-tools"       # Probably have to remove that.
-    Update-Nuget "polymer-core-elements" "prettify-element"
-    Update-Nuget "polymer-core-elements" "sampler-scaffold"
-    Update-Nuget "polymer-core-elements" "web-animations-js"
-    rm polymer-core-elements/content/Scripts/Polymer/web-animations-js/node_modules -Recurse -Force
+function Update-Polymer-Iron-Elements() {
+    Update-Nuget "polymer-iron-a11y-announcer" "iron-a11y-announcer"
+    Update-Nuget "polymer-iron-a11y-keys-behavior" "iron-a11y-keys-behavior"
+    Update-Nuget "polymer-iron-a11y-keys" "iron-a11y-keys"
+    Update-Nuget "polymer-iron-ajax" "iron-ajax"
+    Update-Nuget "polymer-iron-meta" "iron-meta"
+    Update-Nuget "polymer-iron-flex-layout" "iron-iron-flex-layout"
+    Update-Nuget "polymer-iron-behaviors" "iron-iron-behaviors"
+    Update-Nuget "polymer-iron-validator-behavior" "iron-iron-validator-behavior"
+    Update-Nuget "polymer-iron-validatable-behavior" "iron-iron-validatable-behavior"
+    Update-Nuget "polymer-iron-autogrow-textarea" "iron-autogrow-textarea"
+    Update-Nuget "polymer-iron-collapse" "iron-collapse"
+    Update-Nuget "polymer-iron-fit-behavior" "iron-fit-behavior"
+    Update-Nuget "polymer-iron-form" "iron-form"
+    Update-Nuget "polymer-iron-form-element-behavior" "iron-form-element-behavior"
+    Update-Nuget "polymer-iron-icon" "iron-icon"
+    Update-Nuget "polymer-iron-iconset" "iron-iconset"
+    Update-Nuget "polymer-iron-iconset-svg" "iron-iconset-svg"
+    Update-Nuget "polymer-iron-icons" "iron-icons"
+    Update-Nuget "polymer-iron-image" "iron-image"
+    Update-Nuget "polymer-iron-input" "iron-input"
+    Update-Nuget "polymer-iron-jsonp-library" "iron-jsonp-library"
+    Update-Nuget "polymer-iron-localstorage" "iron-localstorage"
+    Update-Nuget "polymer-iron-media-query" "iron-media-query"
+    Update-Nuget "polymer-iron-resizable-behavior" "iron-resizable-behavior"
+    Update-Nuget "polymer-iron-selector" "iron-selector"
+    Update-Nuget "polymer-iron-menu-behavior" "iron-menu-behavior"
+    Update-Nuget "polymer-iron-overlay-behavior" "iron-overlay-behavior"
+    Update-Nuget "polymer-iron-pages" "iron-pages"
+    Update-Nuget "polymer-iron-range-behavior" "iron-range-behavior"
+    Update-Nuget "polymer-iron-signals" "iron-signals"
+    Update-Nuget "polymer-iron-test-helpers" "iron-test-helpers"
 }
 
 function Update-Paper-Elements() {
     $package = "paper-elements"
     Update-Nuget $package "core-a11y-keys"
+    Update-Nuget $package "paper-behaviors"
+    Update-Nuget $package "paper-button"
     Update-Nuget $package "paper-checkbox"
     Update-Nuget $package "paper-dialog"
-    Update-Nuget $package "paper-dropdown"
-    Update-Nuget $package "paper-dropdown-menu"
-    Update-Nuget $package "paper-elements"
+    Update-Nuget $package "paper-dialog-behavior"
+    Update-Nuget $package "paper-dialog-scrollable"
+    Update-Nuget $package "paper-drawer-panel"
+    Update-Nuget $package "paper-fab"
+    Update-Nuget $package "paper-header-panel"
+    Update-Nuget $package "paper-icon-button"
+    Update-Nuget $package "paper-menu-button"
     Update-Nuget $package "paper-input"
     Update-Nuget $package "paper-item"
-    Update-Nuget $package "paper-menu-button"
+    Update-Nuget $package "paper-material"
+    Update-Nuget $package "paper-menu"
     Update-Nuget $package "paper-progress"
     Update-Nuget $package "paper-radio-button"
     Update-Nuget $package "paper-radio-group"
+    Update-Nuget $package "paper-ripple"
     Update-Nuget $package "paper-slider"
     Update-Nuget $package "paper-spinner"
+    Update-Nuget $package "paper-styles"
     Update-Nuget $package "paper-tabs"
     Update-Nuget $package "paper-toast"
     Update-Nuget $package "paper-toggle-button"
+    Update-Nuget $package "paper-toolbar"
+    Update-Nuget $package "paper-scroll-header-panel"
 }
 
 function Generate-Package() {
@@ -122,12 +101,16 @@ function Generate-Package() {
 function Generate-All-Packages() {
     param([string]$version)
     
-	#This package is deprecated.
-    #Generate-Package "polymer-platform" $version --
-    Generate-Package "polymer-cyclic" $version
     Generate-Package "polymer" $version
-    Generate-Package "polymer-core-component-page" $version
-    Generate-Package "polymer-core-elements" $version
+    
+    Generate-Package "polymer-iron-a11y-announcer" $version
+    Generate-Package "polymer-iron-a11y-keys-behavior" $version
+    Generate-Package "polymer-iron-a11y-keys" $version
+    Generate-Package "polymer-iron-ajax" $version
+    Generate-Package "polymer-iron-meta" $version
+    
+    Generate-Package "polymer-iron-component-page" $version
+    Generate-Package "polymer-iron-elements" $version
     Generate-Package "paper-elements" $version
 }
 
@@ -139,27 +122,22 @@ function Push-Package() {
 }
 
 function Update-All() {
-    #This package is deprecated.
-    #Update-Platform
-    Update-Polymer-Cyclic
     Update-Polymer
-    Update-Polymer-Core-Component-Page
-    Update-Polymer-Core-Elements
+    Update-Polymer-Iron-Component-Page
+    Update-Polymer-Iron-Elements
     Update-Paper-Elements
 }
 
 function Push-All-Packages() {
-    $version = "0.5.5"
+    $version = "1.0.0"
     #This package is deprecated.
-    #Push-Package "polymer-platform" $version
-   # Push-Package "polymer-cyclic" $version
-   # Push-Package "polymer" $version
+    Push-Package "polymer" $version
     Push-Package "polymer-core-component-page" $version
     Push-Package "polymer-core-elements" $version
     Push-Package "paper-elements" $version
 }
 
-$version = "0.5.5"
-#Update-All
+$version = "1.0.0"
+Update-All
 #Generate-All-Packages $version
-Push-All-Packages $version
+#Push-All-Packages $version
